f08c3c4759da0d90a18efb75ee56c6946132ecf7
kzw200015 <kzw200015@gmail.com>
2021-09-24 08:53:54 +0800

    Add Send method to Context

--- context.go
+++ context.go
@@ -1,10 +1,22 @@
 package onebot
 
+import "errors"
+
 type Context struct {
 	Bot *Bot
 	Event
 }
 
-func (ctx *Context) Reply() error {
-	return nil
+func (ctx *Context) Send(message ...MessageSegment) (APIResp, error) {
+	switch ctx.PostType {
+	case MessageEvent:
+		switch ctx.MessageType {
+		case PrivateMessage:
+			return ctx.Bot.SendPrivateMessage(ctx.UserId, message...)
+		case GroupMessage:
+			return ctx.Bot.SendGroupMessage(ctx.GroupId, message...)
+		}
+	}
+
+	return APIResp{}, errors.New("不支持的事件类型，仅支持直接回复消息事件")
 }
